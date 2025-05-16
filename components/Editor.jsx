59c25f37d59a45729540e0aa758e63b2dada0465
Arafat-Ahmad-Sheikh <asterisk287@gmail.com>
2025-05-16 19:43:40 +0530

    heading, bead removed edPage

--- components/Editor.jsx
+++ components/Editor.jsx
@@ -3,9 +3,6 @@ import React from 'react'
 const Editor = () => {
   return (
     <div>
-      <div className="diff-item-2">
-              <div className="bg-base-200 grid place-content-center text-9xl font-black">CodeNova</div>
-            </div>
     </div>
   )
 }
