42b0a4e001490acdef7d457e33d89d79964067c5
Arafat-Ahmad-Sheikh <asterisk287@gmail.com>
2025-07-03 10:30:32 +0530

    fixed avatar bug of dicebar

--- components/Client.jsx
+++ components/Client.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Client = ({ username }) => {
+  const avatarUrl = `https://api.dicebear.com/9.x/bottts/svg?seed=${encodeURIComponent(username)}`;
+
   return (
     <div className="flex items-center space-x-3 p-2 rounded-lg hover:bg-base-200 transition">
       <div className="avatar">
-        <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-          <img src={`https://api.dicebear.com/7.x/beam/svg?seed=${username}`}  alt={`avatar ${username}`} />
+        <div className="w-12 h-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 overflow-hidden">
+          <img src={avatarUrl} alt={`avatar ${username}`} />
         </div>
       </div>
       <span className="font-medium text-base-content">{username}</span>
